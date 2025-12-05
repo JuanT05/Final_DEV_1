62a1e769bf25c86bc3dc6c10536d51b87d1f0b1d
Mrr-kat <shidoxxthoka@gmail.com>
2025-12-05 08:43:21 -0500

    actalizacion

--- utils/db.py
+++ utils/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, Text, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import os
