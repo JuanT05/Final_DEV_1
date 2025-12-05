b24edd813a5fac7a7abe0474f876244e6858b07f
JuanT05 <jtsebastiantarazona@gmail.com>
2025-12-05 08:20:36 -0500

    Prueba commit

--- models.py
+++ models.py
@@ -1,4 +1,10 @@
-class Jugador():
+class Jugador(base):
+    __tablename__ = "jugadores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    pais_origen = Column(String)
+    anio_nacimiento = Column(Integer)
     pass
 
 
